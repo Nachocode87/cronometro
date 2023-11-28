117a2066fbf66fa3edd634c7cb5c853d84cbc197
nachocode <lic.medinajimenez@gmail.com>
2023-11-28 15:53:03 -0300

    primer avance

---
+++ js/app.js
@@ -0,0 +1,29 @@
+(function() {
+    let actualizar = function(){
+        const fecha = new Date();
+        const diaF = fecha.getDate();
+        const mesF = fecha.getMonth();
+        const anioF = fecha.getFullYear();
+        const horaF = fecha.getHours();
+        const minutoF = fecha.getMinutes();
+        const segundoF = fecha.getSeconds();
+
+        const dia = document.getElementById('dia');
+        const mes = document.getElementById('mes');
+        const anio = document.getElementById('anio');
+        const hora = document.getElementById('hora');
+        const minuto = document.getElementById('minuto');
+        const segundo = document.getElementById('segundo');
+        
+        dia.textContent = diaF;
+        mes.textContent = mesF;
+        anio.textContent = anioF;
+        hora.textContent = horaF;
+        minuto.textContent = minutoF;
+        segundo.textContent = segundoF;
+    }
+        
+    
+    actualizar();
+    const setActualizar = setInterval(actualizar,1000)
+}())
